2d75c58c24a3d0ff5d984430d5c5d3335d3d42e5
nael49 <leanbarios2018@gmail.com>
2023-11-03 19:58:20 -0300

    creacion de archivos en variables de entorno para conexion de la bbdd

--- conetDataBase.php
+++ conetDataBase.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "estockear";
+include "config/variables-entorno.php";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=" . $db . ";charset=utf8",$username,$password);
     // set the PDO error mode to exception
